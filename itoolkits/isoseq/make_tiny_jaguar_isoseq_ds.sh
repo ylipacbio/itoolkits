09b20f3d0b2b1140e14906949aa4ac337cb584df
Yuan Li <yli@pacificbiosciences.com>
2018-04-30 14:49:54 -0700

    script make_tiny_jaguar_isoseq_ds.sh to make tiny jaguar subreadset

---
+++ itoolkits/isoseq/make_tiny_jaguar_isoseq_ds.sh
@@ -0,0 +1,7 @@
+set -vex
+sr=/pbi/dept/secondary/siv/smrtlink/smrtlink-sms/smrtlink-release_5.1.0.14963/userdata/jobs_root/001/001151/entry-points/06db0526-3c0d-49f1-90e2-7a335178be42.subreadset.xml 
+csv=/pbi/dept/secondary/siv/smrtlink/smrtlink-sms/jobs-root/001/001151/tasks/pbtranscript2tools.tasks.create_workspace-0/workspace/cluster_report.FL_nonFL.csv 
+hq=/pbi/dept/secondary/siv/smrtlink/smrtlink-sms/jobs-root/001/001151/tasks/pbtranscript2tools.tasks.create_workspace-0/workspace/all_arrowed_hq.100_30_0.99.fasta 
+lq=/pbi/dept/secondary/siv/smrtlink/smrtlink-sms/jobs-root/001/001151/tasks/pbtranscript2tools.tasks.create_workspace-0/workspace/all_arrowed_lq.fasta
+
+python subset_isoseq_subreads.py --num_hq_isoforms 4 --num_lq_isoforms 4 $sr $csv $hq $lq  outdir tiny_jaguar
